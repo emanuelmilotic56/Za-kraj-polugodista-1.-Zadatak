3edaccc8a7bce63a8232c33751c00c78efdfab9c
emanuelmilotic56 <58472080+emanuelmilotic56@users.noreply.github.com>
2021-01-17 15:17:39 +0100

    Add files via upload

---
+++ Zadaci za kraj polugodišta-1. Zadatak/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadaci_za_kraj_polugodišta_1.Zadatak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1="Hrvatska ima more";
+
+            string s2="Italija je prvak svijeta";
+
+            string s3;
+        
+            s1=s1.Substring(0,8);
+
+            s2=s2.Substring(Math.Max(0,7))
+                ;
+            s3=s1+s2;
+
+
+
+
+
+        }
+    }
+}
